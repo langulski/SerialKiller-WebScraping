cedd7e7da0c1b11e4fe0b0fc7c088cf3c52dc412
Lucas Angulski <lucas.angulski@gmail.com>
2021-10-07 10:12:24 -0300

    cleaned DB

--- cleandf.py
+++ cleandf.py
@@ -1,32 +1,31 @@
 import pandas as pd
 import numpy as np
+import re
 
-sk_db = pd.read_csv('sk_db.csv')
 
 
 def cleaning():
-    sk_db.drop('index',axis=1,inplace=True)
+    try:
+        for number in range(1,7):
 
-  
-    sk_db.drop('Unnamed: 0',axis=1,inplace=True)
-    for number in range(1,7):
-        try:
+            sk_db.drop('Unnamed: 0',axis=1,inplace=True)
+            sk_db.drop('index',axis=1,inplace=True)
             a=f'index.{number}'
-        
+            
             sk_db.drop(a,axis=1,inplace=True)
-        
-        except:
-            pass
+    except:
+        pass   
+      
     
     
  
 
-def new_numeric_columns():
-    sk_db['Confirmed Victims'] = [re.search("^.{0,3}\d+",str(a)).group() for a in sk_db['Victims:']]
-    sk_db['Possible Victims']= [re.search("(\d+)(?!.*\d)",str(a)).group() for a in sk_db['Victims:']]
-    sk_db['Starting Year of Activity'] = [re.search("^.{0,4}\d+",str(a)).group() for a in sk_db['Years Active:']]
+def new_numeric_columns(df):
+    df['Confirmed Victims'] = [re.search("^.{0,3}\d+",str(a)).group() for a in df['Victims:']]
+    df['Possible Victims']= [re.search("(\d+)(?!.*\d)",str(a)).group() for a in df['Victims:']]
+    df['Starting Year of Activity'] = [re.search("^.{0,4}\d+",str(a)).group() for a in df['Years Active:']]
 
-def df_types():
-    sk_db.iloc[:, np.r_[41:44,51:91]] = sk_db.iloc[:, np.r_[41:44,51:91]].astype('bool')
-    sk_db['Confirmed Victims'] = sk_db['Confirmed Victims'].astype(int)
-    sk_db['Possible Victims'] = sk_db['Possible Victims'].astype(int)
+def df_types(df):
+    df.iloc[:, np.r_[41:44,51:91]] = df.iloc[:, np.r_[41:44,51:91]].astype('bool')
+    df['Confirmed Victims'] = df['Confirmed Victims'].astype(int)
+    df['Possible Victims'] = df['Possible Victims'].astype(int)
